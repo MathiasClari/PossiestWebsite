28cfaf381931a686d9dcb884d1130ce6b60b209b
Mathias <pxlib@proton.me>
2023-01-29 20:54:58 +0100

    Commit

---
+++ app/head.js
@@ -0,0 +1,9 @@
+const Head = () => (
+  <>
+    <title>Possiest Alpha</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="/possiest.ico" />
+  </>
+);
+
+export default Head;
---
+++ constants/index.js
@@ -0,0 +1,92 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/skywars.png',
+    title: 'SkyWars',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/bedwars.png',
+    title: 'BedWars',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/wildlands.png',
+    title: 'Wildlands',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/skylands.png',
+    title: 'SkyLands',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/city.png',
+    title: 'City',
+  },
+];
+
+export const startingFeatures = [
+  'Open Minecraft',
+  'Click Multiplayer',
+    'Click Add Server',
+    'Type in the name of the server',
+    'Type in the IP address of the server (play.possiest.com)',
+  'Click Done',
+    'Enjoy the game, and don\'t forget to invite your friends,'
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'SkyLands',
+    subtitle:
+        'We are working hard on releasing the new mini game called SkyLands',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Update about Wildlands',
+    subtitle:
+        'Wildlands is currently deep in development',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/skylands.png',
+    title: 'Release of SkyLands',
+    subtitle:
+        'Possiest team is working hard on releasing the new mini game called SkyLands',
+  },
+  {
+    imgUrl: '/city.png',
+    title: 'Cities',
+    subtitle:
+        'You ever wanted to play GTA in Minecraft? Well, now you can! Possiest team is working hard on releasing the new mini game called Cities',
+  },
+  {
+    imgUrl: '/wildlands.png',
+    title: 'Possiest team is expanding!',
+    subtitle:
+        'Possiest team is looking for new members to join the team, if you are interested, please contact us',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
